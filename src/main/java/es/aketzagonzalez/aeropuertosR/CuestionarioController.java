c2d64b77c1547649c1c65204038f8b1883abb561
FormedNumber95 <aketza200510@gmail.com>
2024-11-03 13:23:45 +0100

    Initial commit

---
+++ src/main/java/es/aketzagonzalez/aeropuertosR/CuestionarioController.java
@@ -0,0 +1,82 @@
+package es.aketzagonzalez.aeropuertosR;
+
+import es.aketzagonzalez.aeropuertosQ.Temporizador;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CuestionarioController {
+
+    @FXML
+    private Button btnEnviar;
+
+    @FXML
+    private Temporizador temporizador;
+
+    @FXML
+    private TextField txtPregunta1;
+
+    @FXML
+    private TextField txtPregunta10;
+
+    @FXML
+    private TextField txtPregunta11;
+
+    @FXML
+    private TextField txtPregunta12;
+
+    @FXML
+    private TextField txtPregunta13;
+
+    @FXML
+    private TextField txtPregunta14;
+
+    @FXML
+    private TextField txtPregunta15;
+
+    @FXML
+    private TextField txtPregunta16;
+
+    @FXML
+    private TextField txtPregunta17;
+
+    @FXML
+    private TextField txtPregunta18;
+
+    @FXML
+    private TextField txtPregunta19;
+
+    @FXML
+    private TextField txtPregunta2;
+
+    @FXML
+    private TextField txtPregunta20;
+
+    @FXML
+    private TextField txtPregunta3;
+
+    @FXML
+    private TextField txtPregunta4;
+
+    @FXML
+    private TextField txtPregunta5;
+
+    @FXML
+    private TextField txtPregunta6;
+
+    @FXML
+    private TextField txtPregunta7;
+
+    @FXML
+    private TextField txtPregunta8;
+
+    @FXML
+    private TextField txtPregunta9;
+
+    @FXML
+    void accionEnviar(ActionEvent event) {
+
+    }
+
+}
